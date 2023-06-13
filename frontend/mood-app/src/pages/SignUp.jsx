30699b71f62df6f31b3d5be3f9bc668e65585ac9
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2023-06-13 22:49:28 +0100

    handlesignup

--- frontend/mood-app/src/pages/SignUp.jsx
+++ frontend/mood-app/src/pages/SignUp.jsx
@@ -4,36 +4,41 @@ import AllUsers from '../components/AllUsers'
 
 const SignUp = () => {
 
-    const [name, setName] = useState('')
-    const [password, setPassword] = useState()
+    const [signup, setSignup] = useState({
+        name: '',
+        password: '',
+    })
 
     const handleSignup = (e) => {
         e.preventDefault();
-        console.log(name, password);
+
+        let usersLog = []
+        usersLog.push({...usersLog}, signup)
+        console.log(usersLog);
     }
 
   return (
       <div className='register'>
           <div className='signup'>
               <img src="./images/smiley.png" alt="" />
-              <form onClick={handleSignup}>
+              <form onSubmit={handleSignup}>
                   <div className='inputs'>
                       <label htmlFor="name">Name:</label>
                       <input
                           type="text"
                           name='name'
-                          value={name}
+                          value={signup.name}
                           required
-                          onChange={ ((e)=> setName(e.target.value))} />
+                          onChange={ ((e)=> setSignup(e.target.value))} />
                   </div>
                   <div className='inputs'>
                       <label htmlFor="password">Password:</label>
                       <input
                           type="password"
                           name='password'
-                          value={password}
+                          value={signup.password}
                           required
-                          onChange={((e) => setPassword(e.target.value))} />
+                          onChange={((e) => setSignup(e.target.value))} />
                   </div>
                   <button className='signIn-btn' type='submit'>Sign Up</button>
               </form>
