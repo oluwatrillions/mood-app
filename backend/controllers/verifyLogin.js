5e63da6f697ff2358c3ab47a884f6dfeb0f79d2e
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2023-07-14 13:49:02 +0100

    ...

--- backend/controllers/verifyLogin.js
+++ backend/controllers/verifyLogin.js
@@ -5,7 +5,6 @@ dotenv.config({ path: '../controllers/config/.env' })
 const AuthController = async (req, res, next) => {
     const authHeader = req.headers['authorization']
     if (!authHeader) return res.sendStatus(401)
-    console.log(authHeader);
 
     const token = authHeader && authHeader.split(' ')[1]
     jwt.verify(
