883ea4172da41c7c06709d7960f1e88dc0e8aae5
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2023-08-02 18:43:25 +0100

    edit and delete btn

--- frontend/mood-app/src/pages/SinglePost.jsx
+++ frontend/mood-app/src/pages/SinglePost.jsx
@@ -1,20 +1,30 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import './SinglePost.css'
+import { HiDotsHorizontal } from "react-icons/hi"
+
 
 const SinglePost = () => {
 
-    const[singlePost, setSinglePost] = useState({})
-
+    const [singlePost, setSinglePost] = useState({}) 
+    const [isEdit, setIsEdit] = useState(false)
+    const [iseDelete, setIsDelete] = useState(false)
+    const btnRef = useRef(null)
+    const deleteRef = useRef(null)
     const { _id } = useParams()
-    console.log(_id);
+
+    const actionRef = () => {
+        btnRef.current.classList.toggle('show-action')
+    }
+    const deleteBtn = () => {
+        deleteRef.current.classList.add('delete')
+    }
 
     useEffect(() => {
         fetch(`http://localhost:4000/posts/${_id}`)
             .then((response) => response.json())
             .then((data) => {
                 setSinglePost(data)
-                console.log(data);
         })
     }, [_id])
 
@@ -28,10 +38,26 @@ const SinglePost = () => {
                   <h3 className="titled">{singlePost.title}</h3>
                   <h4 className="text">{singlePost.text}</h4>
                   <div className="poster-detail">
-                      <img src="" alt="" />
-                      <div className="poster-name">
-                          <h2 className="name">{singlePost.name}</h2>
-                          <h5 className="postime">{ singlePost.postedAt}</h5>
+                      <div className="post-action">
+                        <img src="" alt="" />
+                        <div className="poster-name">
+                            <h2 className="name">{singlePost.name}</h2>
+                            <h5 className="postime">{ singlePost.postedAt}</h5>
+                        </div>
+                      </div>
+                      <div className="action">
+                          <HiDotsHorizontal className='edit-icon' onClick={actionRef}/>  
+                        <div className="action-btn" ref={ btnRef}>
+                            <h3>Edit</h3>                          
+                            <h3 className='' ref={deleteRef} onClick={deleteBtn}>Delete</h3>   
+                          </div>
+                          <div className="delete">
+                              <h2>Are you sure you want to DELETE this post?</h2>
+                              <div className="answer">
+                                  <h2>YES</h2>
+                                  <h2>NO</h2>
+                              </div>
+                          </div>
                       </div>
                   </div>
               </div>
