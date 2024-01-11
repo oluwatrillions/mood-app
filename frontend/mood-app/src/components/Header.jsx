dc37877817bc54cb79c8b081eea79689c0e0bdd4
Michael Ajose <ajosemichaeloluwatobi@yahoo.com>
2024-01-11 22:09:36 +0100

    refactoring header

--- frontend/mood-app/src/components/Header.jsx
+++ frontend/mood-app/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect, useRef } from 'react'
 import './Header.css'
 import { AiOutlineMenu } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
@@ -7,19 +7,27 @@ const Header = () => {
 
     const { user, handleLogout } = useContext(AuthContext)
 
+    const collapsedRef = useRef()
+
+    const sidebar = () => {
+        collapsed.current.classList.toggle('show-sidebar')
+    }
+
   return (
       <div className='header'>
           {
               user ? 
-                    <nav className='logged-in'>
-                      <AiOutlineMenu className='header-menu' />
+                  <nav className='logged-in'>
+                      <div className="collapse" ref={collapsedRef}>
+                        <AiOutlineMenu className='header-menu' onClick={sidebar}/>
+                      </div>
                         <Link to='/users'><h3 className='users-btn'>Most Recent Users</h3></Link>
-                        <h3 className='welcome'>Welcome <span>{ user.name}</span></h3>
-                        <h3 className='desc'>This is a social media app where users air out their moods</h3>
                         <div className="post-links">
                             <h3><Link to='/posts'> View Posts</Link></h3>  
                             <h3><Link to="/post/create">Post a Message</Link></h3>
                         </div>
+                        <h3 className='desc'>This is a social media app where users air out their moods</h3>
+                        <h3 className='welcome'>Welcome <span>{ user.name}</span></h3>
                         <button onClick={handleLogout}>Logout</button>
                     </nav>
                   :
