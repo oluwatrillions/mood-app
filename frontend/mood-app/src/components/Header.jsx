0d6c8a2e05768c59f1cf6ad9b15fbfa916e1e885
Michael Ajose <ajosemichaeloluwatobi@yahoo.com>
2024-01-13 12:09:58 +0100

    header

--- frontend/mood-app/src/components/Header.jsx
+++ frontend/mood-app/src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     const collapsedRef = useRef()
 
     const sidebar = () => {
-        collapsed.current.classList.toggle('show-sidebar')
+        collapsedRef.current.classList.toggle('show-sidebar')
     }
 
   return (
@@ -18,14 +18,14 @@ const Header = () => {
           {
               user ? 
                   <nav className='logged-in'>
-                      <div className="collapse" ref={collapsedRef}>
+                      <div className="collapse">
                         <AiOutlineMenu className='header-menu' onClick={sidebar}/>
                       </div>
-                        <Link to='/users'><h3 className='users-btn'>Most Recent Users</h3></Link>
-                        <div className="post-links">
-                            <h3><Link to='/posts'> View Posts</Link></h3>  
-                            <h3><Link to="/post/create">Post a Message</Link></h3>
-                        </div>
+                      <div className="show-links" ref={collapsedRef}>
+                            <Link to='/users'><h3 className='users-btn'>Most Recent Users</h3></Link>
+                            <Link to='/posts'><h3> View Posts</h3></Link> 
+                            <Link to="/post/create"><h3>Post a Message</h3> </Link>
+                      </div>
                         <h3 className='desc'>This is a social media app where users air out their moods</h3>
                         <h3 className='welcome'>Welcome <span>{ user.name}</span></h3>
                         <button onClick={handleLogout}>Logout</button>
