af6b1c71ede4089979ceb7075ff38e98af8a31d7
Michael Ajose <ajosemichaeloluwatobi@yahoo.com>
2024-01-31 22:44:18 +0100

    close-btn icon

--- frontend/mood-app/src/components/Header.jsx
+++ frontend/mood-app/src/components/Header.jsx
@@ -3,6 +3,8 @@ import './Header.css'
 import { AiOutlineMenu } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import AuthContext from '../Contexts/AuthContext'
+import { IoIosClose } from "react-icons/io";
+
 const Header = () => {
 
     const { user, handleLogout } = useContext(AuthContext)
@@ -22,6 +24,9 @@ const Header = () => {
                         <AiOutlineMenu className='header-menu' onClick={sidebar}/>
                       </div>
                       <div className="show-links" ref={collapsedRef}>
+                          <div className="close-btn">
+                            <IoIosClose/>
+                          </div>
                             <Link to='/users'><h3 className='users-btn'>Most Recent Users</h3></Link>
                             <Link to='/posts'><h3> View Posts</h3></Link> 
                             <Link to="/post/create"><h3>Post a Message</h3> </Link>
