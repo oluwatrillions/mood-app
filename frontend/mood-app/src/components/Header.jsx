81fc5d2bbf41eddd13ee5be2294fc28658a229dc
oluwatrillions <ajosemichaeloluwatobi@yahoo.com>
2024-08-27 10:48:30 +0100

    ....

--- frontend/mood-app/src/components/Header.jsx
+++ frontend/mood-app/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect, useRef } from 'react'
 import './Header.css'
 import { AiOutlineMenu } from 'react-icons/ai'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import AuthContext from '../Contexts/AuthContext'
 import { IoIosClose } from "react-icons/io";
 
@@ -10,6 +10,9 @@ const Header = () => {
     const { user, handleLogout } = useContext(AuthContext)
 
     const collapsedRef = useRef()
+    const location = useLocation()
+    console.log(location.pathname);
+    
 
     const sidebar = () => {
         collapsedRef.current.classList.toggle('show-sidebar')
@@ -31,10 +34,10 @@ const Header = () => {
                           <div className="close-btn">
                             <IoIosClose onClick={closeSidebar}/>
                           </div>
-                            <Link to='/users'><h3 className='users-btn'>Most Recent Users</h3></Link>
-                            <Link to='/posts'><h3> View Posts</h3></Link> 
-                            <Link to="/post/create"><h3>Post a Message</h3> </Link>
-                            <Link to="/posts/likes"><h3>Likes and Comments</h3> </Link>
+                            <Link to='/users'><h3 className={location.pathname.endsWith("/users") ? "users-btn active" : "users-btn"}>Most Recent Users</h3></Link>
+                            <Link to='/posts'><h3 className={location.pathname.endsWith("/posts") ? "users-btn active" : "users-btn"}> View Posts</h3></Link> 
+                            <Link to="/post/create"><h3 className={location.pathname.endsWith("/post/create") ? "users-btn active" : "users-btn"}>Post a Message</h3> </Link>
+                            <Link to="/posts/likes"><h3 className={location.pathname.endsWith("/posts/likes") ? "users-btn active" : "users-btn"}>Likes and Comments</h3> </Link>
                       </div>
                         <h3 className='desc'>This is a social media app where users air out their moods</h3>
                         <h3 className='welcome'>Welcome <span>{ user.name}</span></h3>
--- frontend/mood-app/src/main.jsx
+++ frontend/mood-app/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {GoogleOAuthProvider} from "@react-oauth/google"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId='1017090253744-3lhf17dd38v83nd3n6finh7n37pg0m2a.apps.googleusercontent.com'>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </GoogleOAuthProvider>
 )
