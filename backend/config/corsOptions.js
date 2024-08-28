519ca3a95b5318beadcb2dd3a09fc64f85d7a338
oluwatrillions <ajosemichaeloluwatobi@yahoo.com>
2024-08-28 02:16:28 +0100

    working on google login

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -3,6 +3,14 @@ const corsOptions = {
     origin: whitelist,
     credentials: true,
     optionsSuccessStatus: 200,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
 };
+
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authortization');
+//     res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+// })
 
 module.exports = corsOptions
