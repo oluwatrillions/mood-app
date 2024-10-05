471e512e022ad5457c0cb5711fcd4e40fb1359b7
oluwatrillions <ajosemichaeloluwatobi@yahoo.com>
2024-10-05 12:17:16 +0100

    ...

--- frontend/mood-app/src/utils/useAxios.js
+++ frontend/mood-app/src/utils/useAxios.js
@@ -12,26 +12,32 @@ const useAxios = () => {
     
     const axiosConfig = axios.create({
         baseURL,
-        headers: { Authorization: ` Bearer ${userToken}` },
+        headers: { 'Content-Type': 'application/json' },
     })
 
     axiosConfig.interceptors.request.use(async req => {
 
-        const user = jwt_decode(userToken)
-        const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1
+        // const user = jwt_decode(userToken)
+        // const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1
 
-        console.log('isExpired', isExpired);
+        // console.log('isExpired', isExpired);
 
-        if (!isExpired) return req
+        // if (!isExpired) return req
 
-        const response = await axios.post(`http://localhost:4000/refreshtoken`, null, {
-            withCredentials: true,
-        });
-        console.log(response);
-        localStorage.setItem('accesstoken', JSON.stringify(response.data))
-        setUserToken(response.data)
-        req.headers.Authorization = `Bearer ${response.data}`
-        return req
+        // const response = await axios.post(`http://localhost:4000/refreshtoken`, null, {
+        //     withCredentials: true,
+        //     credentials: 'include',
+        // });
+        // console.log(response);
+        // localStorage.setItem('accesstoken', JSON.stringify(response.data))
+        // setUserToken(response.data)
+        function (config) {
+            const token = userToken
+            if(token){
+                req.headers.Authorization = `Bearer ${userToken}`
+            }
+            return req
+        }
 
     }, function (error) {
         return Promise.reject(error)
