c3c87e216d1721172e88feb10f946579e7c7b8a0
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2025-01-13 16:05:52 +0100

    ...

--- backend/controllers/refreshTokenController.js
+++ backend/controllers/refreshTokenController.js
@@ -2,31 +2,42 @@ const Users = require("../model/users");
 const jwt = require("jsonwebtoken");
 
 const handleRefreshToken = async (req, res) => {
-  const cookies = req.cookies;
+  try {
+    const cookies = req.cookies;
 
-  if (!cookies?.user)
-    return res.status(401).json({ message: "no user found in cookie" });
-  const refreshToken = cookies.user;
+    if (!cookies?.user)
+      return res.status(401).json({ message: "no user found in cookie" });
+    const refreshToken = cookies.user;
 
-  const user = await Users.findOne({ refreshToken }).exec();
-  if (!user) return res.sendStatus(403);
+    const user = await Users.findOne({ refreshToken }).exec();
+    if (!user) return res.status(403).json({ message: "no valid token found" });
 
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-    if (err || user.email !== decoded.email) {
-      return res.status(403).json({ message: "error from verify token" });
-    }
-    const payload = {
-      name: user.name,
-      username: user.username,
-      email: user.email,
-      avatar: user.profileImage,
-    };
-    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: "180s",
-    });
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      (err, decoded) => {
+        if (err || user.email !== decoded.email) {
+          return res.status(403).json({ message: "Token verification failed" });
+        }
 
-    res.json(accessToken);
-  });
+        const payload = {
+          name: user.name,
+          username: user.username,
+          email: user.email,
+          avatar: user.profileImage,
+        };
+
+        const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "180s",
+        });
+
+        res.json({ accessToken });
+      }
+    );
+  } catch (error) {
+    console.error("Error handling refresh token", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 module.exports = handleRefreshToken;
--- frontend/mood-app/src/utils/useAxios.js
+++ frontend/mood-app/src/utils/useAxios.js
@@ -1,52 +1,72 @@
-import jwtDecode from 'jwt-decode'
-import dayjs from 'dayjs'
-import { useContext } from 'react'
-import AuthContext from '../Contexts/AuthContext'
-import axios from 'axios'
+import jwtDecode from "jwt-decode";
+import dayjs from "dayjs";
+import { useContext } from "react";
+import AuthContext from "../Contexts/AuthContext";
+import axios from "axios";
 
-    const baseURL = 'http://localhost:4000'    
+const baseURL = "http://localhost:4000";
 
-    const useAxios = ()=> {
-        const {userToken, setUserToken, user, setUser, handleLogout} = useContext(AuthContext)
+const useAxios = () => {
+  const { userToken, setUserToken, user, setUser, handleLogout } =
+    useContext(AuthContext);
 
-        const userAccess = localStorage.getItem('accesstoken')
+  const axiosInstance = axios.create({
+    baseURL,
+    credentials: "include",
+    withCredentials: true,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
 
-        const axiosInstance = axios.create({
-            baseURL,
-            credentials: 'include',
-            withCredentials: true,
-            headers: { 
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-             },
-        })           
+  axiosInstance.interceptors.request.use(
+    async (req) => {
+      if (userToken) {
+        req.headers.Authorization = `Bearer ${userToken}`;
+      }
+      //   const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
 
-        axiosInstance.interceptors.request.use(async req => {
-            if(userToken){
-                req.headers.Authorization = `Bearer ${userToken}`
-            };
+      const decodedToken = userToken ? jwtDecode(userToken) : null;
 
-            const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1
-            
-            if(!isExpired) return req
-    
-            const response = await fetch(`${baseURL}/refreshtoken`, {
-                method: 'POST',
-                credentials: 'include',
-            })
-            const newAccess = await response.json();
-            
-            localStorage.setItem('accesstoken', JSON.stringify(newAccess))
+      const isExpired =
+        decodedToken && dayjs.unix(decodedToken.exp).diff(dayjs()) < 10;
 
-            req.headers.Authorization = `Bearer ${newAccess}`
+      if (!isExpired) return req;
 
-            return req
-            
-        }, (error)=> {
-            return Promise.reject(error)
+      try {
+        const response = await fetch(`${baseURL}/refreshtoken`, {
+          method: "POST",
+          credentials: "include",
         });
 
-        return axiosInstance
-    } 
-        
-    export default useAxios
+        if (!response.ok) {
+          handleLogout(); // Logout user if token refresh fails
+          throw new Error("Failed to refresh token");
+        }
+        const newAccess = await response.json();
+        console.log(newAccess);
+
+        const newAccessToken = newAccess.accessToken;
+
+        localStorage.setItem("accesstoken", newAccessToken);
+        setUserToken(newAccessToken);
+
+        req.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return req;
+      } catch (error) {
+        console.log("Error refreshing token", error);
+        handleLogout();
+        return Promise.reject(error);
+      }
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  return axiosInstance;
+};
+
+export default useAxios;
