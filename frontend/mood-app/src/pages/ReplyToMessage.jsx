f0f2d192d078397ecca73d1696650483980712c2
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2025-02-21 13:05:05 +0100

    mq: comment on message

--- frontend/mood-app/src/pages/ReplyToMessage.jsx
+++ frontend/mood-app/src/pages/ReplyToMessage.jsx
@@ -11,15 +11,15 @@ const ReplyToMessage = ({ post, replyAt, replyTo, onReply, userComment, setUserC
   return (
       <div className='reply-div'>
           <form onSubmit={onReply} >
-            <h5>{replyTo }</h5>
-            <h5>Replying to @{replyAt}</h5>
+            <h5 className='reply-msg'>{replyTo }</h5>
+            <h5 className='reply-to'>reply to <span>@{replyAt}</span></h5>
               <input type="text"
                   maxLength="100"
                   name='comment'
                   value={userComment}
                   onChange={(e) => setUserComment(e.target.value)} 
                   />
-              <button>{ userComment.length > 0 ? <h3>Submit Comment</h3> : <h3>Back</h3>}</button>
+              <button>{ userComment.length > 0 ? <h3>submit</h3> : <h3>Back</h3>}</button>
           </form>
     </div>
   )
