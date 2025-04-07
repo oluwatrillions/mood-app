43b46e4e8203194dcc003ebb327791f034aa1155
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2025-04-07 13:18:00 +0100

    ...

--- frontend/mood-app/vite.config.js
+++ frontend/mood-app/vite.config.js
@@ -1,21 +1,31 @@
-import { defineConfig } from 'vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(),
+  plugins: [
+    react(),
     nodePolyfills({
       // To exclude specific polyfills, add them to this list.
-      exclude: ['fs'],
+      exclude: ["fs"],
       // Whether to polyfill specific globals.
       globals: {
         Buffer: true, // can also be 'build', 'dev', or false
         global: true,
         process: true,
       },
+      build: {
+        rollupOptions: {
+          output: {
+            entryFileNames: `assets/[name]-[hash].js`,
+            chunkFileNames: `assets/[name]-[hash].js`,
+            assetFileNames: `assets/[name]-[hash].[ext]`,
+          },
+        },
+      },
       // Whether to polyfill `node:` protocol imports.
       protocolImports: true,
     }),
-    ],
-})
+  ],
+});
